f07c79aedd923dbda5c3291c4f72c21cc60043de
Water075 <yeahidkorb@gmail.com>
2025-05-29 01:33:56 +0100

    main.lua
    check to wait until game is loaded

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+--
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3"))()
 local w = library:CreateWindow("EBHH")
 local b = w:CreateFolder("(v1.0.0)")
